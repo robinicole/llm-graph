d8acdf7dcc3ea6867bf030f9cb938f67c9e16389
Robin Nicole <robin@Robins-MacBook-Air.local>
2024-04-27 11:48:44 +0100

    Make the kg generation generic
    * Add a generic function to generate kg
    * Move the logic to generate the prompt to generate book summary into a separate function
    * Update some typing logic

--- llm_graphs/__init__.py
+++ llm_graphs/__init__.py
@@ -1,4 +1,10 @@
-"""LLM Graphs.
+"""LLM Graphs creator."""
+from __future__ import annotations
 
-Your description here
-"""
+from llm_graphs.main import (
+    draw_kg_with_pyvis,
+    get_book_summary_knowledge_graph,
+    get_knowledge_graph,
+)
+
+__all__ = ['get_knowledge_graph', 'draw_kg_with_pyvis', 'get_book_summary_knowledge_graph']
--- llm_graphs/main.py
+++ llm_graphs/main.py
@@ -2,11 +2,15 @@
 from __future__ import annotations
 
 from functools import lru_cache
-from typing import Any
+from typing import (
+    Any,
+    List,
+)
 
 # Pydantic to specify LLM output schema
 import instructor
 from openai import OpenAI
+from openai.types.chat import ChatCompletionMessageParam  # noqa: TCH002
 
 # Plotting utils
 from pyvis.network import Network
@@ -14,11 +18,41 @@ from pyvis.network import Network
 from llm_graphs.models import KnowledgeGraph
 
 
-@lru_cache(maxsize=None)
-def get_knowledge_graph_object(
-    book_title: str,
+def get_knowledge_graph(
+    messages: List[ChatCompletionMessageParam],
     model: str = 'gpt-4',
 ) -> KnowledgeGraph:
+    """Return a KnowledgeGraph object for a given book title using the OpenAI API.
+
+    The function first generates some text using the OpenAI API, then uses the
+    Pydantic model `KnowledgeGraph` to parse the resulting text as a graph.
+    The function is memoized using functools.lru_cache to avoid generating
+    the same graph multiple times.
+
+    Parameters
+    ----------
+    messages : List[ChatCompletionMessageParam]
+        The messages to send to the OpenAI API
+    model : str, optional
+        The OpenAI API model to use, by default 'gpt-4'
+
+    Returns
+    -------
+    KnowledgeGraph
+        The generated graph representing the book.
+    """
+    if model not in ['gpt-3.5-turbo', 'gpt-4']:
+        raise ValueError('Model must be either gpt-3.5-turbo or gpt-4')
+    client = instructor.from_openai(OpenAI())
+    return client.chat.completions.create(
+        model=model,
+        response_model=KnowledgeGraph,
+        messages=messages,
+    )
+
+
+@lru_cache(maxsize=None)
+def get_book_summary_knowledge_graph(book_title: str, model: str = 'gpt-4') -> KnowledgeGraph:
     """Return a KnowledgeGraph object for a given book title using the OpenAI API.
 
     The function first generates some text using the OpenAI API, then uses the
@@ -31,27 +65,21 @@ def get_knowledge_graph_object(
     book_title : str
         The title of the book to summarize.
     model : str, optional
-        The OpenAI API model to use, by default 'gpt-4'
-    client : OpenAI, optional
-        The OpenAI client to use, by default `instructor.from_openai(OpenAI())`
+        The OpenAI API model to use, by default 'gpt-4'.
 
     Returns
     -------
     KnowledgeGraph
         The generated graph representing the book.
     """
-    client = instructor.from_openai(OpenAI())
-    return client.chat.completions.create(
-        model=model,
-        response_model=KnowledgeGraph,
-        messages=[
-            {
-                'role': 'system',
-                'content': 'You are an avid reader and you summarize the books in knowledge graphs and make it entertaining',  # noqa: E501
-            },
-            {
-                'role': 'user',
-                'content': f'''Summarize the book {book_title} in the graph given as type
+    messages: List[ChatCompletionMessageParam] = [
+        {
+            'role': 'system',
+            'content': 'You are an avid reader and you summarize the books in knowledge graphs and make it entertaining',  # noqa: E501
+        },
+        {
+            'role': 'user',
+            'content': f'''Summarize the book {book_title} in the graph given as type
 Each of the nodes into the graph represent one main concept of the graph
 Each of the link represent a link between two main concept
 the graph should  have 10 nodes and 20 links.
@@ -59,9 +87,9 @@ the resulting graph should be visually appealing and give a good global understa
 Take some time and reason step by step before creating the graph object to make a graph that will be easy to display
 A node should not have self loop and there should not be a loop between two nodes
 The graph should give to the reader a good summary of the book''',
-            },
-        ],
-    )
+        },
+    ]
+    return get_knowledge_graph(messages=messages, model=model)
 
 
 def draw_kg_with_pyvis(knowledge_graph: KnowledgeGraph) -> Any:  # noqa: D417,ANN401
